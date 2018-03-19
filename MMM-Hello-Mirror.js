acd07f88991462e75095964076bc246293b22f29
pdfkpb <pdfkpb@gmail.com>
2018-03-19 17:31:27 -0400

    Creates local copies of annyang and responsive voice

--- MMM-Hello-Mirror.js
+++ MMM-Hello-Mirror.js
@@ -20,8 +20,8 @@ Module.register("MMM-Hello-Mirror", {
 	// Load required additional scripts
 	getScripts: function() {
 		return [
-			'//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js',     // annyang! SpeechRecognition
-			'http://code.responsivevoice.org/responsivevoice.js',                // ResponsiveVoice
+			'annyang.js',     // annyang! SpeechRecognition
+			'responsive_voice.js',                // ResponsiveVoice
 			'moment.js'                                                          // Parse, validate, manipulate, and display dates in JavaScript
 		];
 	},
